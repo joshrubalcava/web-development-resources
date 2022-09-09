a222be852d92fc4a24ab21f53fbe3c1eead30bf0
Josh Rubalcava <josh.rubalcava@yahoo.com>
2022-09-08 19:26:27 -0700

    finished navbar transition and added changes to the styling to make it mobile friendly

--- app.js
+++ app.js
@@ -10,8 +10,36 @@
 //     })
 // }
 
-window.addEventListener('scroll', function(evt) {
-    if (document.body.scrollTop >= 200) {
-        console.log('scrolling')
+// document.addEventListener('DOMContentLoaded', () => {
+//     const navbar = document.querySelector('.navbar-custom');
+//     window.onscroll((evt) => {
+//         navbar.classList.toggle('navbar-light bg-light navbar-red navbar-dark')
+//     })
+// })
+
+// const navbar = document.querySelector('.navbar-custom');
+// navbar.addEventListener('click', () => {
+//     navbar.classList.toggle('navbar-light');
+//     navbar.classList.toggle('bg-light');
+//     navbar.classList.toggle('navbar-red');
+//     navbar.classList.toggle('navbar-dark');
+//     console.log(window);
+// })
+
+const body = document.querySelector('body');
+body.addEventListener('scroll', (evt) => {
+    const navbar = document.querySelector('.navbar');
+    console.log(evt)
+    // console.log(body.scrollTop);
+    if (body.scrollTop >= 300) {
+        navbar.classList.remove('navbar-light');
+        navbar.classList.remove('bg-light');
+        navbar.classList.add('navbar-red');
+        navbar.classList.add('navbar-dark');
+    } else {
+        navbar.classList.add('navbar-light');
+        navbar.classList.add('bg-light');
+        navbar.classList.remove('navbar-red');
+        navbar.classList.remove('navbar-dark');
     }
 })
