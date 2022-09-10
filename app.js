6969255f715659e0a16c5904033e676bee0de32c
Josh Rubalcava <josh.rubalcava@yahoo.com>
2022-09-09 22:09:14 -0700

    finished the subscribe form and linked it to formsubmit.io so that the info gets sent to me and updated some styling

--- app.js
+++ app.js
@@ -1,36 +1,6 @@
-// document.onreadystatechange = function() {
-//     let scrollPosition = 0;
-//     const navbar = document.querySelector('.navbar-custom');
-//     window.addEventListener('scroll', (e) => {
-//         scrollPosition = window.scrollY;
-//         console.log(scrollPosition);
-//         if (scrollPosition > 100) {
-//             navbar.classList.add('navbar-red');
-//         }
-//     })
-// }
-
-// document.addEventListener('DOMContentLoaded', () => {
-//     const navbar = document.querySelector('.navbar-custom');
-//     window.onscroll((evt) => {
-//         navbar.classList.toggle('navbar-light bg-light navbar-red navbar-dark')
-//     })
-// })
-
-// const navbar = document.querySelector('.navbar-custom');
-// navbar.addEventListener('click', () => {
-//     navbar.classList.toggle('navbar-light');
-//     navbar.classList.toggle('bg-light');
-//     navbar.classList.toggle('navbar-red');
-//     navbar.classList.toggle('navbar-dark');
-//     console.log(window);
-// })
-
 const body = document.querySelector('body');
 body.addEventListener('scroll', (evt) => {
     const navbar = document.querySelector('.navbar');
-    console.log(evt)
-    // console.log(body.scrollTop);
     if (body.scrollTop >= 300) {
         navbar.classList.remove('navbar-light');
         navbar.classList.remove('bg-light');
