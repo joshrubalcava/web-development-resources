b571e633b8d372e721c3ab702504f75099e80248
Josh Rubalcava <josh.rubalcava@yahoo.com>
2022-09-10 12:57:43 -0700

    changed subscribe form to netlify and finished styling for mobile devices

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-import axios from "axios";
-
 document.addEventListener('DOMContentLoaded', () => {
     const body = document.querySelector('body');
     body.addEventListener('scroll', (evt) => {
@@ -16,31 +14,6 @@ document.addEventListener('DOMContentLoaded', () => {
             navbar.classList.remove('navbar-dark');
         }
     })
-
-    // const subscribeForm = document.querySelector('.subscribe-form');
-    // subscribeForm.addEventListener('submit', (evt) => {
-    //     evt.preventDefault();
-    //
-    //     subscribeForm.style.display = 'none';
-    //
-    //     const formContainer = document.querySelector('.form-container');
-    //
-    //     const divEl = document.createElement('div');
-    //     divEl.classList.add('thank-you-container');
-    //
-    //     const h1El = document.createElement('h1');
-    //     h1El.innerText = 'Thank you for subscribing!';
-    //     h1El.classList.add('thank-you-title');
-    //
-    //     const h6El = document.createElement('h6');
-    //     h6El.innerText = 'You will receive additional information or a response to your message shortly!';
-    //     h6El.classList.add('confirmation-text')
-    //
-    //     divEl.append(h1El);
-    //     divEl.append(h6El);
-    //
-    //     formContainer.append(divEl);
-    // })
 })
 
 
